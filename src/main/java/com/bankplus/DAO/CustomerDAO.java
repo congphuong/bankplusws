9c6af45198e39797087f67327c35f759efcb28d1
tonidoan96 <bongbaytuoidep@gmail.com>
2017-11-27 15:41:12 +0700

    commit view hisstory wallet

--- src/main/java/com/bankplus/DAO/CustomerDAO.java
+++ src/main/java/com/bankplus/DAO/CustomerDAO.java
@@ -1,13 +1,16 @@
 package com.bankplus.DAO;
 
 import com.bankplus.connection.MyPool;
+import com.bankplus.model.Customer;
 import com.bankplus.model.User;
+import com.bankplus.model.WalletHistory;
 import org.apache.commons.pool.ObjectPool;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class CustomerDAO {
 
@@ -56,8 +59,116 @@ public class CustomerDAO {
         return user;
     }
 
+    public ArrayList lichSuGiaoDich(int idCustomer){
+        WalletHistory wl = null;
+        ArrayList<WalletHistory>list=new ArrayList<>();
+        Connection connection = null;
+        ObjectPool pool = MyPool.getInstance();
+
+        try {
+            connection = (Connection) pool.borrowObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        PreparedStatement ps = null;
+        try {
+            ps = connection.prepareStatement("Select * from Wallet_History where user_id=?");
+            ps.setInt(1, idCustomer);
+
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                wl = new WalletHistory();
+                wl.setWalletHistoryID(rs.getInt(1));
+                wl.setIdCustomer(rs.getInt(2));
+                wl.setChangeType(rs.getInt(3));
+                wl.setChangeCost(rs.getDouble(4));
+                wl.setChangeDate(rs.getTimestamp(5));
+
+                list.add(wl);
+            }
+            rs.close();
+            ps.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (ps != null)
+                    ps.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (connection != null)
+                    pool.returnObject(connection);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return list;
+    }
+
+
+    public boolean signOut(Customer customer){
+        Customer customer1 = null;
+        Connection connection = null;
+        ObjectPool pool = MyPool.getInstance();
+        String result = "";
+        try {
+            connection = (Connection) pool.borrowObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        PreparedStatement ps = null;
+        try {
+            ps = connection.prepareStatement("Select SIGNIN(?,?,?,?,?,?,?) FROM DUAL;");
+            ps.setString(1,customer.getUserName());
+            ps.setString(2, customer.getPassword());
+            ps.setString(3, customer.getIndentify());
+            ps.setString(4, customer.getFirstName());
+            ps.setString(5, customer.getLastName());
+            ps.setString(6, customer.getGender());
+            ps.setString(7, customer.getAddress());
+
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                result = rs.getString(1);
+                if(result=="done"){
+                    return true;
+                }
+            }
+
+            rs.close();
+            ps.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (ps != null)
+                    ps.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (connection != null)
+                    pool.returnObject(connection);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+
+
+        return false;
+    }
+
+
     public static void main(String[] args) {
         CustomerDAO customerDAO = new CustomerDAO();
-        System.out.println(customerDAO.getUserByUsername("trile"));
+        System.out.println(customerDAO.lichSuGiaoDich(1));
     }
 }
